c18e63b8a00cfbfd39050f3f4a43fe497fdaaf15
Meritxell Safont <mscervera@outlook.es>
2022-03-11 18:18:44 +0100

    update

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import './header.css'
+import header_bg from '../../assets/header_bg.jpg'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="travel__header section__padding" id="home">
+      <div className="travel__header-content">
+        <h1>Explore Georgia</h1>
+        <div className="travel__header-image">
+          <img src={header_bg} alt="background-img"/>
+        </div>
+      </div>
+    </div>
   )
 }
 
